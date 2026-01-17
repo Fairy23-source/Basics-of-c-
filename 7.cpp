63a5b416b8fb886696530c133f76447f9ffcf0e3
Fairy Solanki <solankifairy612@gmail.com>
2026-01-17 09:30:23 +0530

    Add files via upload

---
+++ 7.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+//using the concept of constants
+//if we use constant we will not be able to change the value of declared variableand program will give error otherwise possible to make changes without usinf const with variable
+int main (){
+    int a=34;
+    cout<<"the value of a was"<<a;
+    a=55;
+    cout<<"the value of a is"<<a;
+    return 0;
+}
